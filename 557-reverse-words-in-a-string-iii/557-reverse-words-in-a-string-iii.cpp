55008f426cda24018f85d5053f1a051934a419cb
Nilutpal Baruah <72130667+nilutpalbr@users.noreply.github.com>
2022-09-22 11:20:03 +0530

    Time: 65 ms (11.68%), Space: 13.4 MB (20.30%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        queue<string> st2; string temp="";
-        for(int i=0;i<s.length();i++){
+         stack<string> st2; string temp="";
+        for(int i=s.length()-1;i>=0;i--){
             if(s[i]==' '){
-                reverse(temp.begin(),temp.end());
                 st2.push(temp);
                 temp="";
             }
@@ -13,15 +12,16 @@ public:
             }
         }
         string temp2=temp;
-        reverse(temp2.begin(),temp2.end());
         string ans="";
+        st2.push(temp2);
         while(!st2.empty()){
-            string top=st2.front();
+            string top=st2.top();
             st2.pop();
             ans+=top;
             ans+=" ";
         }
-        ans+=temp2;
+        //ans+=temp2;
+        ans.pop_back();
         return ans;
     }
 };
