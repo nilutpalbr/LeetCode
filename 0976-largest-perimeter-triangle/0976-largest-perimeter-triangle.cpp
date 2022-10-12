4181df55cbcd2d97c1b112c4ad97a532056dbdc2
Nilutpal Baruah <72130667+nilutpalbr@users.noreply.github.com>
2022-10-12 16:13:56 +0530

    Time: 93 ms (18.02%), Space: 22 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,21 +1,14 @@
+
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
-        deque<int> window; int sum=0; int max_Sum=0; int start=0;
+        
         sort(nums.begin(),nums.end());
-        for(int i=0;i<3;i++){
-            window.push_back(nums[i]); start++;
+        for(int i = nums.size()-1 ;i>=2;i--){
+            if(nums[i]< nums[i-1]+ nums[i-2]){
+            return nums[i]+nums[i-1]+nums[i-2];
+            }
         }
-        if(nums[0]+nums[1]>nums[2]){sum=nums[0]+nums[1]+nums[2];}
-        max_Sum=max(max_Sum,sum);
-        for(int i=start;i<nums.size();i++){
-            window.pop_front();
-            window.push_back(nums[start++]);
-            int first= window.at(0);
-            int second=window.at(1);
-           if(first+second>window.at(2)){sum=first+second+window.at(2);}
-               max_Sum=max(max_Sum,sum);
-        }
-        return max_Sum;
+        return 0;
     }
 };
