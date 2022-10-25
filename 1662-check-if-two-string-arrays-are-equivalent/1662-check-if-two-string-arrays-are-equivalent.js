6ebed38897c024dc5c35f4368a916db371a6a33f
Nilutpal Baruah <72130667+nilutpalbr@users.noreply.github.com>
2022-10-25 16:09:18 +0530

    Time: 101 ms (36.13%), Space: 41.9 MB (77.88%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    let string1=""; let string2=""
+    for(let i=0;i<word1.length;i++){
+        string1+=word1[i];
+    }
+     for(let i=0;i<word2.length;i++){
+        string2+=word2[i];
+    }
+    return string1==string2
+};
